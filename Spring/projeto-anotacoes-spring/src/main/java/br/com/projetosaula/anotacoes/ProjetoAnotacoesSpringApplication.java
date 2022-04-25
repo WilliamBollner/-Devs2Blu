a50073a70c318a1affee957899f31813a2588f49
William Bollner <williambollner@hotmail.com>
2022-04-25 17:25:19 -0300

    Add outra versão do projeto anotações

---
+++ Spring/projeto-anotacoes-spring/src/main/java/br/com/projetosaula/anotacoes/ProjetoAnotacoesSpringApplication.java
@@ -0,0 +1,13 @@
+package br.com.projetosaula.anotacoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoAnotacoesSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoAnotacoesSpringApplication.class, args);
+	}
+
+}
